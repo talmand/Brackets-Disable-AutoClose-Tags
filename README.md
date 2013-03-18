d0bfe22cbee86f27427996430e378b215cdf71ff
Travis Almand <travisalmand@yahoo.com>
2013-03-18 11:46:25 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-Brackets-Disable-AutoClose-Tags
-===============================
+Brackets Disable AutoClose Tags extension
+===
+Brackets extension that will disable the AutoClose Tags "whenOpening" feature of Codemirror.
 
-Brackets extension that offers option to disable autoclosing of HTML tags on "whenOpening".
+What this means is that it does not turn off all auto closing of HTML tags in Brackets. It turns off the auto closing when you create the opening tag of an element.
+
+Here's an example. If you type this:
+
+&lt;p&gt;
+
+By default, Brackets will take over and close it with this:
+
+&lt;p&gt;&lt;/p&gt;
+
+Then place the cursor between the opening and closing tags of the element. This extension prevents that part. But if you type this:
+
+&lt;p&gt;this is text&lt;/
+
+Brackets will then close the rest of the tag like this:
+
+&lt;p&gt;this is text&lt;/p&gt;
+
+I prefer this behavior and before I would always edit the editor.js file in Brackets to change that setting. I eventually grew tired of having to do that with every version of Brackets so I decided to make an extension until Brackets has a proper preferences menu.
+
+This extension has a checked entry under "Edit" in the main menu. It saves your preference to file so you can turn it on and off as needed and Brackets will start using your last choice. At first I wasn't going to do this making the change somewhat permanent but I decided to make it an option.
+
+A similar idea could be used to turn off all auto closing of HTML tags. For this extension that would require a second entry in the menu or some sort of preferences modal.
+
+To install this extension:
+In Brackets, under "Help" select "Show Extensions Folder". Place extension folder with files inside the "user" folder.
+Older versions of Brackets this choice might be under "Debug" or might not exist at all.
+
+
+Usage
+=====
+Check or uncheck "Disable AutoClose Tags" under Edit.
+
+
+Known issues
+=====
+Possibly not an issue, but changes the settings on any editor as it is opened regardless if it is HTML or not.
---
+++ main.js
@@ -0,0 +1,99 @@
+/*
+* Copyright (c) 2013 Travis Almand. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, browser: true */
+/*global define, brackets, $, action */
+
+define(function (require, exports, module) {
+    
+    'use strict';
+
+    var AppInit = brackets.getModule("utils/AppInit"),
+        CommandManager = brackets.getModule("command/CommandManager"),
+        EditorManager = brackets.getModule("editor/EditorManager"),
+        NativeFileSystem = brackets.getModule("file/NativeFileSystem").NativeFileSystem,
+        FileUtils = brackets.getModule("file/FileUtils"),
+        Menus = brackets.getModule("command/Menus");
+    
+    var file, fileEntry, settings;
+    
+    file = FileUtils.getNativeModuleDirectoryPath(module) + "/settings.json";
+    fileEntry = new NativeFileSystem.FileEntry(file);
+    
+    function changeSettings() {
+        // first we change the check in the menu, then update the settings, refresh the editor to apply changes
+        CommandManager.get("disableAutoCloseTags").setChecked(settings.disableAutoCloseTags);
+        // pflynn pointed out a bug in CodeMirror requiring this null setting before applying the changes
+        EditorManager.getCurrentFullEditor()._codeMirror.setOption("autoCloseTags", null);
+        EditorManager.getCurrentFullEditor()._codeMirror.setOption("autoCloseTags", {indentTags: [], whenClosing: true, whenOpening: !settings.disableAutoCloseTags});
+        EditorManager.getCurrentFullEditor().refresh();
+    }
+    
+    function getSettings() {
+        // settings hasn't been loaded? get them before continuing
+        // otherwise just move forward
+        if (!settings) {
+            var readPromise = FileUtils.readAsText(fileEntry);
+            readPromise.done(function (text) {
+                if (text) {
+                    settings = $.parseJSON(text);
+                    changeSettings();
+                }
+            }).fail(function (error) {
+                console.log(error);
+                settings = {"disableAutoCloseTags": false};
+            });
+        } else {
+            changeSettings();
+        }
+    }
+    
+    function action() {
+        // save the new choice and move forward
+        var writeSettings;
+        if (CommandManager.get("disableAutoCloseTags").getChecked()) {
+            settings = {"disableAutoCloseTags": false};
+            writeSettings = "{\"disableAutoCloseTags\": false}";
+        } else {
+            settings = {"disableAutoCloseTags": true};
+            writeSettings = "{\"disableAutoCloseTags\": true}";
+        }
+        var writePromise = FileUtils.writeText(fileEntry, writeSettings);
+        writePromise.done(function () {
+            getSettings();
+        }).fail(function (error) {
+            console.log(error);
+        });
+    }
+    
+    $(EditorManager).on("activeEditorChange", function () {
+        if (EditorManager.getCurrentFullEditor()) {
+            getSettings();
+        }
+    });
+    
+    CommandManager.register("Disable AutoClose Tags", "disableAutoCloseTags", action);
+    var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+    menu.addMenuDivider();
+    menu.addMenuItem("disableAutoCloseTags");
+    
+});
